49fe3aa0c1bb2c4fc7c52ccd7c32c513dcbee326
TrazZed <hayestristan26@gmail.com>
2024-03-14 23:01:44 +1000

    Implemented Skeleton Code

---
+++ image_to_ascii.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+
+def asciiConvert(image, type, saveas, scale):
+    scale = int(scale)
+    
+    #Open and get image size
+    img = Image.open(image)
+    w,h = img.size
+
+    #Resize image (Downscale)
+    img.resize((w//scale, h//scale)).save("resized.%s" % type)
+
+    #Open new image
+    img = Image.open("resized.%s" % type)
+    w,h = img.size
+
+def main():
+    print("OK")
+
+
+if __name__ == "__main__":
+    main()
