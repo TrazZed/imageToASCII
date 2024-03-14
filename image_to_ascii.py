9c6b679ee51912acca89693da265ee94d725c3a9
TrazZed <hayestristan26@gmail.com>
2024-03-14 23:27:05 +1000

    Updated Examples

--- image_to_ascii.py
+++ image_to_ascii.py
@@ -53,7 +53,7 @@ def asciiConvert(image, type, saveas, scale):
     art.close()
 
 def main():
-    asciiConvert("smolmewithaward.jpg", "jpg", "cursed.txt", "3")
+    asciiConvert("smolmewithaward.jpg", "jpg", "cursed.txt", "6")
 
 if __name__ == "__main__":
     main()
