52b12a77878c8dc22502ea26f2dd4683458574c5
TrazZed <hayestristan26@gmail.com>
2024-03-14 23:21:24 +1000

    Fully Implemented

--- image_to_ascii.py
+++ image_to_ascii.py
@@ -13,11 +13,47 @@ def asciiConvert(image, type, saveas, scale):
 
     #Open new image
     img = Image.open("resized.%s" % type)
-    w,h = img.size
+    w,h = img.size #new width and height
+
+    #List with the correct length and height (same as resized image)
+    grid = []
+    for i in range(h):
+        grid.append(["X"] * w)
+
+    pix = img.load()
+
+    for y in range(h):
+        for x in range(w):
+            if sum(pix[x,y]) == 0:
+                grid[y][x] = "#" 
+            elif sum(pix[x,y]) in range(1,100):
+                grid[y][x] = "X"
+            elif sum(pix[x,y]) in range(100, 200):
+                grid[y][x] = "%"
+            elif sum(pix[x,y]) in range(200, 300):
+                grid[y][x] = "&"
+            elif sum(pix[x,y]) in range(300, 400):
+                grid[y][x] = "*"
+            elif sum(pix[x,y]) in range(400, 500):
+                grid[y][x] = "+"
+            elif sum(pix[x,y]) in range(500, 600):
+                grid[y][x] = "/"
+            elif sum(pix[x,y]) in range(600, 700):
+                grid[y][x] = "("
+            elif sum(pix[x,y]) in range(700, 750):
+                grid[y][x] = "'"
+            else:
+                grid[y][x] = " "
+
+    art = open(saveas, "w")
+
+    for row in grid:
+        art.write("".join(row)+"\n")
+
+    art.close()
 
 def main():
-    print("test")
-
+    asciiConvert("smolmewithaward.jpg", "jpg", "cursed.txt", "3")
 
 if __name__ == "__main__":
     main()
